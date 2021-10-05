d6cd401f0a6111f1bfca5f3b60ea692ffb6c4710
T1masvTP <timotei2002@gmail.com>
2021-10-05 18:16:26 +0200

    test de commit

---
+++ tgerante.c
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]) {
+  pid_t pid;
+  int i;
+  FILE *fich = NULL;
+
+  fich = fopen("nombres.txt", "a");
+  pid = fork();
+
+  if (pid == 0){
+    for (size_t i = 0; i < 6; i++) {
+      fprintf(fich , "%d" ,i );
+    }
+    exit(0);
+  }
+  for (size_t i = 6; i < 10; i++) {
+    fprintf(fich, "%d",i );
+  }
+  exit(0);
+
+}
